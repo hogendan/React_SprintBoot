52a55d44cf8a3cae236c8f238171915033c6d27e
hogendan <85877911+hogendan@users.noreply.github.com>
2024-09-02 14:23:38 +0900

    feat: Click Event 追加

--- react-todo-demo/react-todos/src/App.js
+++ react-todo-demo/react-todos/src/App.js
@@ -8,12 +8,26 @@ function App() {
     { rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "User One" },
   ];
 
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: "New Todo",
+        rowAssigned: "User Three",
+      };
+      todos.push(newTodo);
+    }
+  };
+
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
           <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
     </div>
