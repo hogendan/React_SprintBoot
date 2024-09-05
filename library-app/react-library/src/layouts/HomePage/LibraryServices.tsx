b62c81f23446bd8c28043b97535ad16a4db794ec
hogendan <85877911+hogendan@users.noreply.github.com>
2024-09-05 11:25:27 +0900

    feat: Add LibraryServices Component

---
+++ library-app/react-library/src/layouts/HomePage/LibraryServices.tsx
@@ -0,0 +1,23 @@
+export const LibraryServices = () => {
+  return (
+    <div className="contaienr my-5">
+      <div className="row p-4 align-items-center border shadow-lg">
+        <div className="col-lg-7 p-3">
+          <h1 className="display-4 fw-bold">
+            Can't find what you are looing for?
+          </h1>
+          <p className="lead">
+            If you cannnot find what you are looking for, send our library
+            admin's a personal message!
+          </p>
+          <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
+            <a href="#" className="btn main-color btn-lg text-white">
+              Sign up
+            </a>
+          </div>
+        </div>
+        <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
+      </div>
+    </div>
+  );
+};
