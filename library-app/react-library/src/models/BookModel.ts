51f7fa86c318827195ef63749de2f2cf3ec20ad7
hogendan <85877911+hogendan@users.noreply.github.com>
2024-09-06 14:13:12 +0900

    feat: Add Book Model

---
+++ library-app/react-library/src/models/BookModel.ts
@@ -0,0 +1,25 @@
+class BookMdodel {
+    id: number;
+    title: string;
+    author?: string;
+    description?: string;
+    copies?: string;
+    copiesAvailable?: number;
+    category?: string;
+    img?: string;
+
+    constructor(id: number, title: string, author: string, description: string, copies: string,
+        copiesAvailable: number, category: string, img: string
+    ) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.copies = copies;
+        this.copiesAvailable = copiesAvailable;
+        this.category = category;
+        this.img = img;
+    }
+}
+
+export default BookMdodel;
